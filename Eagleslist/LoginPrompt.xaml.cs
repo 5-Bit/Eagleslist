ebb392cfaa179c5081616b96342667dccf0c1634
Mick MacCallum <micksmaccallum@gmail.com>
2015-10-27 01:09:42 -0400

    Started login prompt

---
+++ Eagleslist/LoginPrompt.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Eagleslist
+{
+    public partial class LoginPrompt : Window
+    {
+        public LoginPrompt()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            Close();
+        }
+    }
+}
