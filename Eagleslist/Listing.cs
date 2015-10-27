486e77c7635650f5252d9d11564ad603bea1a2ba
Mick MacCallum <micksmaccallum@gmail.com>
2015-10-27 01:11:01 -0400

    Updated Listing class to match web service response.

--- Eagleslist/Listing.cs
+++ Eagleslist/Listing.cs
@@ -9,15 +9,31 @@ namespace Eagleslist
     public class Listing
     {
         public string Title { get; private set; }
+        public string Content { get; private set; }
+        public string Handle { get; private set; }
+        public string Email { get; private set; }
+        public string Bio { get; private set; }
+        public string ImageURL { get; private set; }
+        public string Price { get; private set; }
+        public string Condition { get; private set; }
 
-        public Listing(string title)
+        public int ListingID { get; private set; }
+        public int UserID { get; private set; }
+
+        public Listing(string title, string Content, string Handle,
+            string Email, string Bio, string ImageURL, string Price, string Condition,
+            int ListingID, int UserID)
         {
             this.Title = title;
-        }
-
-        public override string ToString()
-        {
-            return this.Title;
+            this.Content = Content;
+            this.Handle = Handle;
+            this.Email = Email;
+            this.Bio = Bio;
+            this.ImageURL = ImageURL;
+            this.Price = Price;
+            this.Condition = Condition;
+            this.ListingID = ListingID;
+            this.UserID = UserID;
         }
     }
 }
