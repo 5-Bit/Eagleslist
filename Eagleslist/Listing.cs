acf2de85787f6fd96cdacd7dd8873f5ea3b92819
Mick MacCallum <micksmaccallum@gmail.com>
2015-11-03 19:49:49 -0500

    Added field for creation date

--- Eagleslist/Listing.cs
+++ Eagleslist/Listing.cs
@@ -16,13 +16,14 @@ namespace Eagleslist
         public string ImageURL { get; private set; }
         public string Price { get; private set; }
         public string Condition { get; private set; }
+        public string CreateDate { get; private set; }
 
         public int ListingID { get; private set; }
         public int UserID { get; private set; }
 
         public Listing(string title, string Content, string Handle,
             string Email, string Bio, string ImageURL, string Price, string Condition,
-            int ListingID, int UserID)
+            int ListingID, int UserID, string CreateDate)
         {
             this.Title = title;
             this.Content = Content;
@@ -34,6 +35,7 @@ namespace Eagleslist
             this.Condition = Condition;
             this.ListingID = ListingID;
             this.UserID = UserID;
+            this.CreateDate = CreateDate;
         }
     }
 }
