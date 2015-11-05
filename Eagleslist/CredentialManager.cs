2e1eb51673da1be1bd835b47a248c92675621454
Mick MacCallum <micksmaccallum@gmail.com>
2015-11-05 13:39:51 -0500

    Added CredentialManager class

---
+++ Eagleslist/CredentialManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eagleslist
+{
+    public class CredentialManager
+    {
+        public static User getCurrentUser()
+        {
+            return new User(0, "session", "", "msmaccallum5438@eagle.fgcu.edu", "0x7fffffff", "bio", true, false);
+        }
+
+        public static void setCurrentUser(User user)
+        {
+
+        }
+    }
+}
