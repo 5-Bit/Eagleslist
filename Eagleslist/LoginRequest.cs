7f0db0350906ea06a684835bd9712c503aaadfac
Mick MacCallum <micksmaccallum@gmail.com>
2015-11-06 17:47:41 -0500

    Adde LoginRequest class

---
+++ Eagleslist/LoginRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eagleslist
+{
+    public class LoginRequest
+    {
+        public string UserHandle { get; private set; }
+        public string Password { get; private set; }
+
+        public LoginRequest(string UserHandle, string Password)
+        {
+            this.UserHandle = UserHandle;
+            this.Password = Password;
+        }
+    }
+}
