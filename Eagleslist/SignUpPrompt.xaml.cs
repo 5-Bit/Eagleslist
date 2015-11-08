b4bb2d352eef289a283317911505c785c3b85b02
Mick MacCallum <micksmaccallum@gmail.com>
2015-11-07 19:37:50 -0500

    hooked up sign up events

--- Eagleslist/SignUpPrompt.xaml.cs
+++ Eagleslist/SignUpPrompt.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace Eagleslist
 {
@@ -10,6 +13,39 @@ namespace Eagleslist
         public SignUpPrompt()
         {
             InitializeComponent();
+        }
+
+        private void ResendVerificationEmailClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartUsingEagleslistClicked(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            Close();
+        }
+
+        private void SignUpButtonClicked(object sender, RoutedEventArgs e)
+        {
+            AttemptRegistration();
+        }
+
+        private async void AttemptRegistration()
+        {
+            RegistrationSubmission submission = new RegistrationSubmission(
+                handleField.Text, passwordField.Password, emailField.Text
+            );
+
+            User createdUser = await RequestManager.AttemptRegistration(submission);
+
+            Console.WriteLine(createdUser);
+        }
+
+        private void CollapseSignInFields()
+        {
+            TopGrid.Visibility = Visibility.Collapsed;
+            BottomGrid.Visibility = Visibility.Visible;
         }
     }
 }
