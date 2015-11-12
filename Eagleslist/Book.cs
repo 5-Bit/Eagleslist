bf55c7aca80421473a427a2d46845221b194a125
Mick MacCallum <micksmaccallum@gmail.com>
2015-11-11 21:55:08 -0500

    Formatting

--- Eagleslist/Book.cs
+++ Eagleslist/Book.cs
@@ -4,16 +4,10 @@ namespace Eagleslist
     public class Book 
     {
         public int BookID { get; private set; }
-
         public string ISBN10 { get; private set; }
-
         public string ISBN13 { get; private set; }
-
         public string Title { get; private set; }
-
         public string ImageURL { get; private set; }
-
         public string Description { get; private set; }
-       
     }
 }
