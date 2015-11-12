b7f6904817575610543bf18efaca908a497fc330
MAnsary9277467 <msansary6258@eagle.fgcu.edu>
2015-11-11 21:50:50 -0500

    Added the Flag Handler Request

---
+++ Eagleslist/FlagHandlerRequest.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Eagleslist
+{
+    public class FlagHandlerRequest
+    {
+        public FlagResolution Marking { get; private set; }
+        public string Message { get; private set; }
+        public int PostID { get; private set; }
+    }
+}
