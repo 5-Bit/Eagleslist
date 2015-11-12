9e5948f365e8ba80f15e24649f941e6a2c41612a
Mick MacCallum <micksmaccallum@gmail.com>
2015-11-12 10:33:57 -0500

    Started sign up validation

--- Eagleslist/SignUpPrompt.xaml.cs
+++ Eagleslist/SignUpPrompt.xaml.cs
@@ -1,13 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Animation;
 
 namespace Eagleslist
 {
-    /// <summary>
-    /// Interaction logic for SignUpPrompt.xaml
-    /// </summary>
     public partial class SignUpPrompt : Window
     {
         public SignUpPrompt()
@@ -54,6 +51,65 @@ namespace Eagleslist
         {
             TopGrid.Visibility = Visibility.Collapsed;
             BottomGrid.Visibility = Visibility.Visible;
+        }
+
+        private void UserInputChanged(object sender, RoutedEventArgs e)
+        {
+            List<string> errors = Validate();
+
+            if (errors.Count == 0)
+            {
+                InputErrorTextBox.Height = 0;
+                InputErrorTextBox.Text = null;
+                SignUpButton.IsEnabled = true;
+            }
+            else
+            {
+                SignUpButton.IsEnabled = false;
+                InputErrorTextBox.Height = Double.NaN;
+                InputErrorTextBox.Text = String.Join<string>("\n\n", errors);
+            }
+        }
+
+        private List<string> Validate()
+        {
+            List<string> errors = new List<string>();
+
+            if (string.IsNullOrEmpty(emailField.Text.Trim()))
+            {
+                errors.Add("Email field can not be empty");
+            }
+            else
+            {
+                //Regex pattern = new Regex("^[a-zA-Z]+[0-9]*@(eagle.)?fgcu.edu$");
+                //bool isEmailValid = emailField.Text != null && pattern.IsMatch(emailField.Text);
+                bool isEmailValid = emailField.Text.EndsWith("@eagle.fgcu.edu")
+                    || emailField.Text.EndsWith("@fgcu.edu");
+                if (!isEmailValid)
+                {
+                    errors.Add("Email must be a valid FGCU email address");
+                }
+            }
+
+            if (string.IsNullOrEmpty(handleField.Text.Trim()))
+            {
+                errors.Add("Username field can not be empty");
+            }
+
+            if (string.IsNullOrEmpty(passwordField.Password.Trim())
+                || string.IsNullOrEmpty(confirmPasswordField.Password.Trim()))
+            {
+                errors.Add("Password fields can not be empty");
+            }
+            else
+            {
+                if (!passwordField.Password.Equals(confirmPasswordField.Password))
+                {
+                    errors.Add("Passwords do not match");
+                }
+            }
+
+            return errors;
         }
     }
 }
