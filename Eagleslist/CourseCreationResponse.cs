1b0994b1dcc88e71d7ac2495628d3541b274e126
MAnsary9277467 <msansary6258@eagle.fgcu.edu>
2015-11-11 22:06:24 -0500

    Added Course Creation Response

---
+++ Eagleslist/CourseCreationResponse.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Eagleslist
+{
+    public class CourseCreationResponse
+    {
+        public string Error { get; private set; }
+        public int CourseID { get; private set; }
+    }
+}
