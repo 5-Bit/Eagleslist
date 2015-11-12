dba1c2e07f4a0a61a0d05181a0261386890feedd
MAnsary9277467 <msansary6258@eagle.fgcu.edu>
2015-11-11 21:47:20 -0500

    Added the Flag Raised Response

---
+++ Eagleslist/FlagRaisedResponse.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Eagleslist
+{
+    public class FlagRaisedResponse
+    {
+        public string Error { get; private set; }
+        public string Message { get; private set; }
+    }
+}
