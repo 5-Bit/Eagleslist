72eda98751c5c414333221eb1c6a673c99f51662
Hermilo Calderon <hcalder@eagle.fgcu.edu>
2015-11-11 21:48:11 -0500

    Added Flag Class

---
+++ Eagleslist/Flag.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Eagleslist
+{
+    public class Flag
+    {
+        public User Raiser { get; private set; }
+        public int PostID { get; private set; }
+        public string Content { get; private set; }
+        public FlagType Type { get; private set; }
+    }
+}
