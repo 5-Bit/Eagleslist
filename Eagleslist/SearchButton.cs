6cf1745ec9653ba7cf8b641fdb2f9eab39c711c3
Michael MacCallum <micksmaccallum@gmail.com>
2015-11-22 02:28:24 -0500

    Added SearchButton class

---
+++ Eagleslist/SearchButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Eagleslist
+{
+    public class SearchButton : Button
+    {
+        private bool _isSelected = false;
+        public bool isSelected
+        {
+            get
+            {
+                return _isSelected;
+            }
+
+            set
+            {
+                _isSelected = value;
+                var color = value ? "#006F41" : "#00885A"; 
+                Background = Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(color));
+            }
+        }
+    }
+}
