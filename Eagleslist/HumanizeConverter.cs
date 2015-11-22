294a126f201dec99d74f7c72ec3a5836d58f93cf
Michael MacCallum <micksmaccallum@gmail.com>
2015-11-22 02:27:38 -0500

    Added HumanizeConverter

---
+++ Eagleslist/HumanizeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Humanizer;
+
+namespace Eagleslist
+{
+    public class HumanizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            var date = value as DateTime?;
+
+            if (date.HasValue && date.Value != null)
+            {
+                return date.Value.Humanize(false);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
