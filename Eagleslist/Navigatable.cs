92151fa729b6c04a17fb56ee86141c3399307634
Michael MacCallum <micksmaccallum@gmail.com>
2015-11-23 21:27:29 -0500

    Started adding new navigation stack

---
+++ Eagleslist/Navigatable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eagleslist
+{
+    public interface Navigatable<T, U> where T : new()
+    {
+        T LoadFromNavigation(U load);
+    }
+}
