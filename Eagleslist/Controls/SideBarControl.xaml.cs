b4b2807cdc920dc6bed66f9eac4383cff69e560c
Michael MacCallum <micksmaccallum@gmail.com>
2015-11-24 19:24:35 -0500

    Added navigation associations and removed old nav logic

--- Eagleslist/Controls/SideBarControl.xaml.cs
+++ Eagleslist/Controls/SideBarControl.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Collections.Generic;
-using System;
 
 namespace Eagleslist.Controls
 {
@@ -10,48 +8,19 @@ namespace Eagleslist.Controls
     /// </summary>
     public partial class SideBarControl : UserControl
     {
-        private MainWindow _container;
-        internal MainWindow ContainingWindow
-        {
-            get
-            {
-                return _container;
-            }
-            set
-            {
-                _container = value;
-
-                List<Button> buttons = new List<Button> {
-                    SearchButton, ComposeButton, ListingsButton, CoursesButton
-                };
-
-                if (value?.PrimaryNavigationControls.Count < buttons.Count)
-                {
-                    value.PrimaryNavigationControls.InsertRange(0, buttons);
-                }
-            }
-        }
-
         public SideBarControl()
         {
             InitializeComponent();
-            SelectControlButton(SearchButton);
+
+            NavigationManager.AddAssociation<SearchControl>(SearchButton);
+            NavigationManager.AddAssociation<ListingCreationControl>(ComposeButton);
+            NavigationManager.AddAssociation<ListingBrowserControl>(ListingsButton);
+            NavigationManager.AddAssociation<CoursesBrowserControl>(CoursesButton);
         }
 
         private void NavigationButtonClicked(object sender, RoutedEventArgs e)
         {
-            SelectControlButton((Button)sender);
-        }
-
-        internal void SelectControlButton(Button button)
-        {
-            int tag = int.Parse(button.Tag.ToString());
-            ContainingWindow?.ContainerDisplayPanelAtIndex(tag);
-        }
-
-        private void SearchButtonClicked(object sender, RoutedEventArgs e)
-        {
-            ContainingWindow.SearchButtonClicked(sender as SearchButton);
+            NavigationManager.NavigateFromClick(sender as Button, null);
         }
     }
 }
