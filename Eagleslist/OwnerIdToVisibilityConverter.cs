eb8bec2f0623035f656c3856ff9444ea5a6c6e6a
Michael MacCallum <micksmaccallum@gmail.com>
2015-12-01 13:19:58 -0500

    Added OwnerIdToVisbilityConverter

---
+++ Eagleslist/OwnerIdToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Eagleslist
+{
+    public class OwnerIdToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            var testId = value as int?;
+
+            if (testId.HasValue && testId.Value > 0)
+            {
+                var id = CredentialManager.GetCurrentUser()?.ID;
+
+                if (id.HasValue)
+                {
+                    return id.Value == testId.Value ? Visibility.Visible : Visibility.Collapsed;
+                }
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
