200e7177ebbc048732282e019625836f9b87c453
Michael MacCallum <micksmaccallum@gmail.com>
2015-12-06 15:50:52 -0500

    Added GoogleBookResponse class

---
+++ Eagleslist/GoogleBookResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Eagleslist
+{
+    public class GoogleBookResponse
+    {
+        public string kind { get; set; }
+        public int totalItems { get; set; }
+        public List<GoogleBook> items { get; set; }
+    }
+}
